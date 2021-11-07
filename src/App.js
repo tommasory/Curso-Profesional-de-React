0ab7112ce4869731c592c5d85997f2fa0fd4a92b
Tommasory <tommasory@gmail.com>
2021-11-07 10:19:46 -0500

    Agrega Json de peliculas y lo muestra por medio de un Map, el cual es para recorrer arrays y retornar los elementos

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
+import peliculasJson from './peliculas.json'
 
 function App() {
+
+  let peliculas = peliculasJson;
   
+  // * Utilizamos el MAP para recorrer el vector de peliculas ya que este nos permite ir por cada uno de los elementos y retornandolo a la vez.
+  // * Si se pone llaves hay que especificar el return, de lo contrario no.
   return (
     <PageWrapper>
-
-      <Pelicula titulo="Tomas el ultimo humano" calificacion="10" director="Maria Rocio Cueltan" actores="Heydi Escobar, Freider Escobar, Tomas Escobar" fecha="1 May 2020" duracion="3h22" img="images/uploads/mv1.jpg">
-        Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...
-      </Pelicula>
+      {peliculas.map(pelicula =>
+        <Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion} director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion} img={pelicula.img}>
+          {pelicula.descripcion}
+        </Pelicula>
+      )}
 
     </PageWrapper>
   );
